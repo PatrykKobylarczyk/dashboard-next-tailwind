036d48d6c3649a9aabbbac71041de00ac8755beb
Patryk Kobylarczyk <patryk.kobylarczyk@gmail.com>
2023-02-22 22:18:01 +0100

    work with dvh-part3

--- components/BlendaForMobileMenu.tsx
+++ components/BlendaForMobileMenu.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const BlendaForMobileMenu = () => {
   return (
     <div>
-      <div className="ios h-screen w-3 fixed top-0 left-0 bg-[#D5D0FF] z-[41]"></div>
+      <div className="h-[100dvh] w-3 fixed top-0 left-0 bg-[#D5D0FF] z-[41]"></div>
       <div className="w-full h-10 fixed top-0 left-0 bg-[#D5D0FF] z-[41]"></div>
     </div>
   )
