3886b85d2e37e22ed7d4df7c4d86cd72781e4244
Patryk Kobylarczyk <patryk.kobylarczyk@gmail.com>
2023-03-12 17:04:58 +0100

    add input for searching - customers, companies

--- pages/companies.tsx
+++ pages/companies.tsx
@@ -2,21 +2,67 @@ import BasicCard from "@/components/BasicCard";
 import PageContent from "@/components/PageContent";
 import { companiesData } from "@/data/companiesData";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { BiExport, BiImport } from "react-icons/bi";
 import { BsClockFill } from "react-icons/bs";
 import { FiSearch } from "react-icons/fi";
 import { HiOutlinePlus } from "react-icons/hi";
 import { TfiDownload } from "react-icons/tfi";
 
+export const Company = ({ company }: any) => {
+  return (
+    <BasicCard>
+      <div className="w-full flex flex-col justify-center items-center gap-4 pt-10">
+        <Image src={company.logo} alt={company.name} width={60} height={60} />
+        <h2 className="font-bold">{company.name}</h2>
+        <p className="text-center pb-3">{company.description}</p>
+        <span className="w-full h-[1px] bg-gray-100" />
+        <div className="w-full flex gap-2 justify-between items-center">
+          <div className="flex gap-2 items-center">
+            <BsClockFill />
+            <p>{company.updated}</p>
+          </div>
+          <div className="flex gap-2 items-center">
+            <TfiDownload />
+            <p>{company.downloads}</p>
+          </div>
+        </div>
+      </div>
+    </BasicCard>
+  );
+};
+
 const Companies = () => {
+  const [inputSearchCompanyValue, setInputSearchCompanyValue] = useState("");
+
+  // input Search Customer handler
+  const handleChangeSearchInput = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setInputSearchCompanyValue(event.target.value);
+  };
+
+  // searching customer
+  const filteredCompanies = companiesData.filter((company): any => {
+    if (inputSearchCompanyValue.length === 0) {
+      return companiesData;
+    }
+    if (inputSearchCompanyValue.length > 0) {
+      return (
+        inputSearchCompanyValue.trim().toLowerCase() ===
+        company.name
+          .trim()
+          .toLowerCase()
+          .slice(0, inputSearchCompanyValue.length)
+      );
+    }
+  });
+
   return (
     <PageContent>
       <div className="w-full h-auto flex flex-col gap-3 sm:gap-4">
         <div className="w-full flex justify-between items-center">
-          <h2 className="font-bold text-2xl lg:text-3xl my-6">
-            Companies
-          </h2>
+          <h2 className="font-bold text-2xl lg:text-3xl my-6">Companies</h2>
           <button className="h-10 text-white bg-Primary rounded-xl px-3">
             <HiOutlinePlus />
             Add
@@ -41,38 +87,15 @@ const Companies = () => {
               type="search"
               placeholder="search the customer"
               className="w-full sm:w-1/2 md:w-1/3 border-2 border-gray-200 px-8 py-2 rounded-xl placeholder:'search the customer' focus:border-Primary focus:border-2 transition duration-300 outline-none before:absolute before:content-['search'] "
+              onChange={handleChangeSearchInput}
             />
           </form>
         </BasicCard>
       </div>
       <div className="w-full h-auto grid grid-col-1 xs:grid-col-2 md:grid-cols-3 gap-5 mt-10">
-        {companiesData.map((company): any => {
-          return (
-            <BasicCard key={company.id}>
-              <div className="w-full flex flex-col justify-center items-center gap-4 pt-10">
-                <Image
-                  src={company.logo}
-                  alt={company.name}
-                  width={60}
-                  height={60}
-                />
-                <h2 className="font-bold">{company.name}</h2>
-                <p className="text-center pb-3">{company.description}</p>
-                <span className="w-full h-[1px] bg-gray-100" />
-                <div className="w-full flex gap-2 justify-between items-center">
-                  <div className="flex gap-2 items-center">
-                    <BsClockFill />
-                    <p>{company.updated}</p>
-                  </div>
-                  <div className="flex gap-2 items-center">
-                    <TfiDownload />
-                    <p>{company.downloads}</p>
-                  </div>
-                </div>
-              </div>
-            </BasicCard>
-          );
-        })}
+        {filteredCompanies.map((company): any => (
+          <Company key={company.id} company={company} />
+        ))}
       </div>
     </PageContent>
   );
--- pages/customers.tsx
+++ pages/customers.tsx
@@ -22,27 +22,52 @@ interface Customer {
 
 const Customers = () => {
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const [customerPerPage, setCustomerPerPage] = useState<number>(3);
+  const customerPerPage = 8;
+  const [inputSearchCustomerValue, setInputSearchCustomerValue] = useState("");
+
+  // input Search Customer handler
+  const handleChangeSearchInput = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setInputSearchCustomerValue(event.target.value);
+  };
+
+  // searching customer
+  const filteredCustomers = customersData.filter((customer): any => {
+    if (inputSearchCustomerValue.length === 0) {
+      return customersData;
+    }
+    if (inputSearchCustomerValue.length > 0) {
+      return (
+        inputSearchCustomerValue.trim().toLowerCase() ===
+        customer.name
+          .trim()
+          .toLowerCase()
+          .slice(0, inputSearchCustomerValue.length)
+      );
+    }
+  });
+
+  console.log(filteredCustomers);
 
   // Get current customers
   const indexOfLastCustomer = currentPage * customerPerPage;
   const indexOfFirstCustomer = indexOfLastCustomer - customerPerPage;
-  const currentCustomers = customersData.slice(
+  const currentCustomers = filteredCustomers.slice(
     indexOfFirstCustomer,
     indexOfLastCustomer
   );
+
   const data = currentCustomers.map((item: Customer) => (
     <CustomersTableRow key={item.id} item={item} />
   ));
 
   return (
     <PageContent>
-      <div className="w-full h-full flex flex-col gap-3 sm:gap-4 overflow-hidden">
-        <div className="w-full flex justify-between">
-          <h2 className="font-bold text-2xl lg:text-3xl my-6">
-            Customers
-          </h2>
-          <button className="text-white bg-Primary rounded-xl px-3">
+      <div className="page w-full h-full flex flex-col gap-3 sm:gap-4 overflow-x-hidden">
+        <div className="w-full flex justify-between items-center">
+          <h2 className="font-bold text-2xl lg:text-3xl my-6">Customers</h2>
+          <button className="text-white bg-Primary rounded-xl px-3 md:px-5 h-10 md:h-14">
             <HiOutlinePlus />
             Add
           </button>
@@ -57,8 +82,9 @@ const Customers = () => {
             Export
           </button>
         </div>
+
         <BasicCard>
-          <form className="relative w-auto flex items-center">
+          <form className="relative w-auto flex items-center ">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none text-gray-400">
               <FiSearch />
             </div>
@@ -66,9 +92,11 @@ const Customers = () => {
               type="search"
               placeholder="search the customer"
               className="w-full sm:w-1/2 md:w-1/3 border-2 border-gray-200 px-8 py-2 rounded-xl placeholder:'search the customer' focus:border-Primary focus:border-2 transition duration-300 outline-none before:absolute before:content-['search'] "
+              onChange={handleChangeSearchInput}
             />
           </form>
         </BasicCard>
+
         <BasicCard resetPadding>
           <div className="w-full pb-2 overflow-x-auto">
             <table className="w-full min-w-[600px] ">
@@ -83,14 +111,14 @@ const Customers = () => {
                   <td className="categoryTable rounded-tr-2xl">signed up</td>
                 </tr>
               </tbody>
-              <tbody className="w-full overflow-x-auto">{data}</tbody>
+              <tbody className="w-full overflow-x-auto text-center">{filteredCustomers.length!==0 ? data :<p className="pt-5 pb-3">Not found</p>}</tbody>
             </table>
           </div>
         </BasicCard>
         <BasicCard>
           <CustomerPagination
             currentPage={currentPage}
-            total={customersData.length}
+            total={filteredCustomers.length}
             customerPerPage={customerPerPage}
             onPageChange={(page: number) => setCurrentPage(page)}
           />
